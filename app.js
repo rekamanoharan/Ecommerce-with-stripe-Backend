c5ed05c0f6874d9e2d21dece770e2c54cc236c50
rekamanoharan <rekamano15@gmail.com>
2024-02-15 20:28:34 +0530

    ecommerce-stripe-backend

---
+++ app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
+
+// checkout api
+app.post("/api/create-checkout-session", async (req, res) => {
+  const { products } = req.body;
+
+  const lineItems = products.map((product) => ({
+    price_data: {
+      currency: "usd",
+      product_data: {
+        name: product.dish,
+        images: [product.imgdata],
+      },
+      unit_amount: product.price * 100,
+    },
+    quantity: product.qnty,
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: lineItems,
+    mode: "payment",
+    success_url: process.env.CLIENT_URL + "/sucess",
+    cancel_url: process.env.CLIENT_URL + "/cancel",
+  });
+
+  res.json({ id: session.id });
+});
+
+app.listen(process.env.PORT || 3002, () => {
+  console.log("server start");
+});
